f95e988bc605195950e6ac279f7d1b2b7e94b74f
rushiaher1503 <rushiaher1503@gmail.com>
2026-07-01 17:03:04 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,38 @@
+a = 40
+b = 50 
+c = 5 + 6j
+
+print("numeric data types: ")
+print(type(a))
+print(type(b))
+print (type(c))
+
+# sequence types
+a = "Hello"
+b = [1, 2, 3, 4, "rushi", "jay"]
+c = [10, 20, 30]
+
+print("sequence Data types: ")
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Example of set data type
+x = 20
+y = 40
+
+result = x + y
+
+print ("result :", result)
+print ("Data types of result : ", type(result))
+
+# types of variable
+
+name_str = "rushi"
+print("strint to list :", list(name_str))
+
+my_list = [12, 13, 14]
+print("list to tupe: ", tuple(my_list))
+
+call_log = "8010578823"
+print("string to list: ", list(str(call_log)))
