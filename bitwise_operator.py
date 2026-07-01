eac73143191757de8cdd63210244f1cd3cb24337
rushiaher1503 <rushiaher1503@gmail.com>
2026-07-01 16:14:02 +0530

    first commit

---
+++ bitwise_operator.py
@@ -0,0 +1,239 @@
+# 1
+
+print ("bitwise operators")
+a= 10 
+b = 20
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise XOR:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("bitwise left shift:", a << 2)
+print ("bitwise right shift:", a >> 2)
+
+# 2
+
+print ("bitwies oprtators")
+a = 40
+b = 50
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise XOR:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("bitwise left shift:", a << b)
+print ("bitwise right shift:", a >> b)
+
+# 3 
+
+print ("bitwise operators")
+a = 15
+b = 25
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+# 4
+
+print ("bitwise operators")
+a = 35
+b = 55
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+ 
+ # 5
+
+print ("bitwise operators")
+a = 70
+b = 75
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 6
+
+print ("bitwise operators")
+a = 35
+b = 40
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 7
+
+print ("bitwise operators")
+a = 80
+b = 40
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 8
+
+print ("bitwise operators")
+a = 20
+b = 50
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 9
+
+print ("bitwise operators")
+a = 30
+b = 60
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 10
+
+print ("bitwise operators")
+a = 30
+b = 10
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 11
+
+print ("bitwise operators")
+a = 65
+b = 85
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+  # 12
+
+print ("bitwise operators")
+a = 10
+b = 40
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+ 
+  # 13
+   
+print ("bitwise operators")
+a = 65
+b = 20
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 14
+
+print ("bitwise operators")
+a = 80
+b = 30
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+  # 15
+
+print ("bitwise operators")
+a = 20
+b = 40
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 16
+
+print ("bitwise operators")
+a = 95
+b = 45
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 17
+
+print ("bitwise operators")
+a = 40
+b = 70
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 18
+
+print ("bitwise operators")
+a = 20
+b = 35
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 19
+
+print ("bitwise operators")
+a = 60
+b = 75
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
+
+ # 20
+
+print ("bitwise operators")
+a = 75
+b = 85
+print ("bitwise AND:", a & b)
+print ("bitwise OR:", a | b)
+print ("bitwise NOT:", a ^ b)
+print ("bitwise not of a:", ~a)
+print ("left shift:", a << b)
+print ("right shift:", a >> b)
